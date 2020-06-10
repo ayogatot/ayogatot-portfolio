ce7eece76d803bb1c9945645d47c7170af8813bb
ayogatot <azerino25@gmail.com>
2020-06-10 21:56:46 +0700

    refactor socmed

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -15,41 +15,38 @@ function Contact() {
           </p>
           <img className="Phone" src={Phone} alt="Phone" />
           <Row className="pt-4">
-            <Col md="6">
-              <p className="C3 pt-1">Follow Me at</p>
-            </Col>
-            <Col className="text-right Socmed" md="6">
+            <Col className="Socmed" md="6" xs="6">
               <a
                 className="Icon"
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://www.facebook.com/azerino.gatot"
               >
-                <i className="fab fa-facebook ml-2" />
+                <i className="fab fa-facebook" />
               </a>
               <a
-                className="Icon"
+                className="Icon ml-2"
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://www.linkedin.com/in/azerino-yogananta-gatot-subroto-47514716b/"
               >
-                <i className="fab fa-linkedin ml-2" />
+                <i className="fab fa-linkedin" />
               </a>
               <a
-                className="Icon"
+                className="Icon ml-2"
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://twitter.com/ayogatot"
               >
-                <i className="fab fa-twitter-square ml-2" />
+                <i className="fab fa-twitter-square" />
               </a>
               <a
-                className="Icon"
+                className="Icon ml-2"
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://www.instagram.com/ayogatot/"
               >
-                <i className="fab fa-instagram ml-2" />
+                <i className="fab fa-instagram" />
               </a>
             </Col>
           </Row>
